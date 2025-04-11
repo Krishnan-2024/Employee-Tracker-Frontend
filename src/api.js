709963ba447f5233b2299b219c4c7898d3e42d8d
Krishnan S <sivakrishnas785@gmail.com>
2025-04-11 10:01:51 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const getToken = () => localStorage.getItem("access_token");
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://backend-jtcd.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
@@ -31,7 +31,7 @@ api.interceptors.response.use(
         }
 
         // Request new access token
-        const res = await axios.post("http://localhost:8000/api/user/token/refresh/", {
+        const res = await axios.post("https://backend-jtcd.onrender.com", {
           refresh: refreshToken,
         });
 
